2c89f2f4ef4c126eec7dba8d997e89b252b09315
AhmedAKhan <khanaahmed10@gmail.com>
2016-03-25 02:01:26 -0400

    added the check object and check html function

--- test/test.js
+++ test/test.js
@@ -1,96 +1,110 @@
 
+//load the chai tester stuff
 var chai = require('chai');
+var should = chai.should();
+var expect = chai.expect;
 
-var should = chai.should() //actually call the function
+// load all the files
 var fs = require("fs");
 var normalizedPath = require("path").join(__dirname, "jadeimp");
 
-fs.readdirSync(normalizedPath).forEach(function(file) {
-  testFile(file);
+// the jade code
+var jadeimp = require("../index");
+
+
+
+
+// chai has expect({--}).deep.equal({--}); which does the same thing
+// except for our purposes we do not want it to check the text variable
+// check if two variables are the same
+function checkEqual(obj1, obj2){
+  var type = typeof(obj2);
+  if(Array.isArray(obj2)) type = 'array';
+  expect(obj1).to.be.an(type);
+  var type = typeof(obj1);
+  if(type === 'object'){
+    if(Array.isArray(obj1)){// order should not matter
+      expect(obj1).to.have.length(obj2.length);
+      for(var i = 0; i < obj1.length; i++){
+        expect(obj1[i]).to.be.equal(obj2[i]); // order matters
+        // expect(obj1).to.include(obj2[i]); // order does not matter
+      }
+    }else{
+      expect(Object.keys(obj1)).to.have.length(Object.keys(obj2)); // make sure the object's have the same number of length
+      for(var key in Object.keys(obj1)){
+        if(key == "text") continue;
+        checkEqual(obj1[key], obj2[key]);
+      }
+    }
+    return;
+  }
+  expect(obj1).to.be.equal(obj2);
+}
+
+
+// check if the two html strings are the same
+function checkhtml(html1, html2){
+  expect(html1).to.be.equal(html2); // just check if the two strings are equal
+}
+
+// check the checkEqual function
+describe("testing the checkEqual function that is inside the tester", function(){
+  it("check with numbers", function( done ){      checkEqual(4,4); done (); });
+  it("check different numbers", function( done ){ try{ checkEqual(4,20); }catch(e){ done(); }});
+
+  it("check with string", function(done){ checkEqual("hello","hello");    done (); });
+  it("check different string", function(done){ try{ checkEqual("hello","not hello"); }catch(e){ done(); }});
+
+  it("check with array", function(done ){ checkEqual([1,2,3],[1,2,3]);    done (); });
+  it("check different array", function(done ){ try{ checkEqual([1,2,3],[1,2,3,4,5,6]);}catch(e){ done(); }});
+
+  it("check with objects", function(done){ checkEqual({},{}); done (); });
+  it("check with objects", function(done){ try{ checkEqual({},{'a':1}); }catch(e){ done (); }});
 });
 
-function testFile(filename){
 
-  var testname = filename.substr(0, filename.indexOf("."));
+
+// function to test one test case
+function testFile(filename){
+  var testname = filename.substr(0, filename.indexOf(".")); // remove the extension
   console.log("got the filename: " + filename);
 
   // run the test case
   describe("going to test " + testname, function(){
     var jadeCode;
-    var expectedHtml;
+    var expectedJson;
     var expectedHtml;
 
     beforeEach(function(){
-        // load the files
-        expectedJson = fs.readFileSync("./test/tree/"+testname + ".json");
-        expectedHtml = fs.readFileSync("./test/html/"+testname+".html");
-        jadeCode     = fs.readFileSync("./test/jadeimp/"+testname+".jimp");
+      // load the files
+      var expectedJsonString = fs.readFileSync("./test/tree/"+testname + ".json");
+      expectedHtml = fs.readFileSync("./test/html/"+testname+".html");
+      jadeCode     = fs.readFileSync("./test/jadeimp/"+testname+".jimp");
 
-        // parse the file
-        // console.log("expectedJson: " + expectedJson);
-         expectedJson = JSON.parse(expectedJson);
+      // parse the file
+      expectedJson = JSON.parse(expectedJsonString);
     });
-
-    // it("going to try to find the file " + testname, function(done){
-      // try{
-      //   jadeCode     = fs.readFileSync("./test/jadeimp/"+testname+".jimp");
-      // }catch(e){ throw("could not find the jade file, going to move to the next file"); }
-
-      // try{
-      //   expectedHtml = fs.readFileSync("./test/html/"+testname+".html");
-      // }catch(e){ throw("could not find the html file, going to move to the next file"); }
-
-      // try{
-      //   expectedJson = fs.readFileSync("./test/tree/"+testname + ".json");
-      // }catch(e){ throw("could not find the tree file, going to move to the next file"); }
-      // done();
-    // });
 
     // conver the file to a tree
     it("converting the file to tree", function(done){
-      console.log("3. jadeCode: "+ jadeCode);
+      var actualJsonResponse = jadeimp.parse(jadeCode);
+      // checkEqual(expectedJson, actualJsonResponse);
+      checkEqual(expectedJson, expectedJson); // just for testing
       done();
     })
 
     // convert the tree to html
     it("converting the tree to html", function(done){
+      var actualHtml = jadeimp.synthesis(expectedJson);
+      // checkHtml(expectedHtml, actualHtml);
+      checkHtml(expectedHtml, expectedHtml);
       done();
     })
-
   })
 }
 
-describe("asdasd", function(){
 
-  it("test 0", function(done){
-    var expect = require('chai').expect
-      , foo = 'bar'
-      , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
-
-    expect(foo).to.be.a('string');
-    expect(foo).to.equal('bar');
-    expect(foo).to.have.length(3);
-    expect(beverages).to.have.property('tea').with.length(3);
-    done();
-  });
-
-
-  it("test 1", function(done){
-    var foo = 'bar'
-    , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
-  foo.should.be.a('string');
-  foo.should.equal('bar');
-  foo.should.have.length(3);
-  beverages.should.have.property('tea').with.length(3);
-
-  it("inner test", function(done){ done();  });
-
-  done();
-  })
+//load all the files and run the test case
+fs.readdirSync(normalizedPath).forEach(function(file) {
+  testFile(file);
 });
-
-
-
-
-
-
